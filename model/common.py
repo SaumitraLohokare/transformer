efc7acc491a7174d3e3df906ad52e1c3ea8e8b41
SaumitraLohokare <saumitra.lohokare@gmail.com>
2023-10-02 03:26:57 +0530

    Added flash attention to decoder

--- model/common.py
+++ model/common.py
@@ -4,6 +4,7 @@ from torch.nn import functional as F
 
 import math
 
+
 class LayerNorm(nn.Module):
     def __init__(self, embed_dim):
         super().__init__()
@@ -13,19 +14,21 @@ class LayerNorm(nn.Module):
     def forward(self, inputs):
         return F.layer_norm(inputs, self.weight.shape, self.weight, self.bias, 1e-5)
 
+
 class TransformerEmbeddings(nn.Module):
     def __init__(self, vocab_size, embed_dim, max_seq_len):
         super().__init__()
 
         self.tok_embed = nn.Embedding(vocab_size, embed_dim)
         self.pos_embed = nn.Embedding(max_seq_len, embed_dim)
-    
+
     def forward(self, ids):
-        assert len(ids.size()) == 2, "Expected input ids to be of size (batches, sequence_len)"
+        assert len(
+            ids.size()) == 2, "Expected input ids to be of size (batches, sequence_len)"
 
         _, seq_len = ids.size()
 
-        # Create the tensor for positional embeddings        
+        # Create the tensor for positional embeddings
         positions = torch.arange(0, seq_len, dtype=torch.long)
         positions = self.pos_embed(positions)
 
@@ -34,6 +37,7 @@ class TransformerEmbeddings(nn.Module):
         inputs = inputs + positions
 
         return inputs
+
 
 class SelfAttention(nn.Module):
     def __init__(self, embed_dim, max_seq_len, n_heads, is_causal=False):
@@ -47,33 +51,77 @@ class SelfAttention(nn.Module):
 
         self.qkv_layer = nn.Linear(embed_dim, 3 * embed_dim, bias=None)
 
-        if is_causal: # Will this work?
-            self.raw_mask = torch.tril(torch.ones(max_seq_len, max_seq_len)).view(1, 1, max_seq_len, max_seq_len)
-
+        if is_causal:  # Will this work?
+            self.raw_mask = torch.tril(torch.ones(max_seq_len, max_seq_len)).view(
+                1, 1, max_seq_len, max_seq_len)
 
     def forward(self, inputs):
         batches, seq_len, _ = inputs.size()
 
-        resid = inputs.clone() # TODO: Is a clone really necessary here?
+        resid = inputs.clone()  # TODO: Is a clone really necessary here?
         inputs = self.ln(inputs)
 
         q, k, v = self.qkv_layer(inputs).split(self.embed_dim, dim=2)
 
-        q = q.view(batches, seq_len, self.n_heads, self.embed_dim // self.n_heads).transpose(1, 2)
-        k = k.view(batches, seq_len, self.n_heads, self.embed_dim // self.n_heads).transpose(1, 2)
-        v = v.view(batches, seq_len, self.n_heads, self.embed_dim // self.n_heads).transpose(1, 2)
+        q = q.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+        k = k.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+        v = v.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
 
-        attn = (q @ k.transpose(-2, -1)) / math.sqrt(self.embed_dim // self.n_heads)
+        attn = (q @ k.transpose(-2, -1)) / \
+            math.sqrt(self.embed_dim // self.n_heads)
 
         if self.is_causal:
-            attn = attn.masked_fill(self.raw_mask[:,:,:seq_len,:seq_len] == 0, float('-inf'))
-        
+            attn = attn.masked_fill(
+                self.raw_mask[:, :, :seq_len, :seq_len] == 0, float('-inf'))
+
         attn = F.softmax(attn, dim=-1)
 
-        outputs = (attn @ v).transpose(1, 2).contiguous().view(batches, seq_len, self.embed_dim)
+        outputs = (attn @ v).transpose(1,
+                                       2).contiguous().view(batches, seq_len, self.embed_dim)
         outputs = outputs + resid
 
         return outputs
+
+
+class FlashSelfAttention(nn.Module):
+    def __init__(self, embed_dim, n_heads, is_causal=False):
+        super().__init__()
+
+        self.embed_dim = embed_dim
+        self.n_heads = n_heads
+        self.is_causal = is_causal
+
+        self.ln = LayerNorm(embed_dim)
+
+        self.qkv_layer = nn.Linear(embed_dim, 3 * embed_dim, bias=None)
+
+    def forward(self, inputs):
+        batches, seq_len, _ = inputs.size()
+
+        resid = inputs.clone()  # TODO: Is a clone really necessary here?
+        inputs = self.ln(inputs)
+
+        q, k, v = self.qkv_layer(inputs).split(self.embed_dim, dim=2)
+
+        q = q.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+        k = k.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+        v = v.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+
+        outputs = torch.nn.functional.scaled_dot_product_attention(
+            q, k, v, attn_mask=None, dropout_p=0., is_causal=self.is_causal)
+
+        outputs = outputs.transpose(1, 2).contiguous().view(
+            batches, seq_len, self.embed_dim)
+        outputs = outputs + resid
+
+        return outputs
+
 
 class CrossAttention(nn.Module):
     def __init__(self, embed_dim, n_heads):
@@ -88,28 +136,75 @@ class CrossAttention(nn.Module):
         self.q_layer = nn.Linear(embed_dim, embed_dim, bias=None)
 
     def forward(self, inputs, cross_embedings):
-        assert inputs.size()[-1] == cross_embedings.size()[-1], "Both embeddings in cross attention must have the same embedding dimension"
+        assert inputs.size()[-1] == cross_embedings.size(
+        )[-1], "Both embeddings in cross attention must have the same embedding dimension"
 
         batches, seq_len, _ = inputs.size()
-        resid = inputs.clone() # TODO: Is this clone necessary?
+        resid = inputs.clone()  # TODO: Is this clone necessary?
 
         inputs = self.ln(inputs)
         k, v = self.kv_layer(cross_embedings).split(self.embed_dim, dim=2)
         q = self.q_layer(inputs)
 
-        q = q.view(batches, seq_len, self.n_heads, self.embed_dim // self.n_heads).transpose(1, 2)
-        k = k.view(batches, seq_len, self.n_heads, self.embed_dim // self.n_heads).transpose(1, 2)
-        v = v.view(batches, seq_len, self.n_heads, self.embed_dim // self.n_heads).transpose(1, 2)
+        q = q.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+        k = k.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+        v = v.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
 
-        attn = (q @ k.transpose(-2, -1)) / math.sqrt(self.embed_dim // self.n_heads)
+        attn = (q @ k.transpose(-2, -1)) / \
+            math.sqrt(self.embed_dim // self.n_heads)
         attn = F.softmax(attn, dim=-1)
         outputs = attn @ v
-        outputs = outputs.transpose(1, 2).contiguous().view(batches, seq_len, self.embed_dim)
+        outputs = outputs.transpose(1, 2).contiguous().view(
+            batches, seq_len, self.embed_dim)
 
         outputs = outputs + resid
 
         return outputs
-    
+
+
+class FlashCrossAttention(nn.Module):
+    def __init__(self, embed_dim, n_heads):
+        super().__init__()
+
+        self.embed_dim = embed_dim
+        self.n_heads = n_heads
+
+        self.ln = LayerNorm(embed_dim)
+
+        self.kv_layer = nn.Linear(embed_dim, 2 * embed_dim, bias=None)
+        self.q_layer = nn.Linear(embed_dim, embed_dim, bias=None)
+
+    def forward(self, inputs, cross_embedings):
+        assert inputs.size()[-1] == cross_embedings.size(
+        )[-1], "Both embeddings in cross attention must have the same embedding dimension"
+
+        batches, seq_len, _ = inputs.size()
+        resid = inputs.clone()  # TODO: Is this clone necessary?
+
+        inputs = self.ln(inputs)
+        k, v = self.kv_layer(cross_embedings).split(self.embed_dim, dim=2)
+        q = self.q_layer(inputs)
+
+        q = q.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+        k = k.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+        v = v.view(batches, seq_len, self.n_heads,
+                   self.embed_dim // self.n_heads).transpose(1, 2)
+
+        outputs = torch.nn.functional.scaled_dot_product_attention(
+            q, k, v, attn_mask=None, dropout_p=0., is_causal=False)
+        outputs = outputs.transpose(1, 2).contiguous().view(
+            batches, seq_len, self.embed_dim)
+
+        outputs = outputs + resid
+
+        return outputs
+
+
 class FeedForward(nn.Module):
     def __init__(self, embed_dim, hidden_size):
         super().__init__()
@@ -120,10 +215,10 @@ class FeedForward(nn.Module):
         self.expand_layer = nn.Linear(embed_dim, hidden_size, bias=None)
         self.gelu = nn.GELU()
         self.shrink_layer = nn.Linear(hidden_size, embed_dim, bias=None)
-    
+
     def forward(self, inputs):
         resid = inputs.clone()
-        
+
         outputs = self.ln(inputs)
         outputs = self.expand_layer(outputs)
         outputs = self.gelu(outputs)
--- model/decoder.py
+++ model/decoder.py
@@ -4,17 +4,41 @@ from torch.nn import functional as F
 
 import math
 
-from common import SelfAttention, CrossAttention, FeedForward
+from common import SelfAttention, CrossAttention, FeedForward, FlashSelfAttention, FlashCrossAttention
+
 
 class DecoderBlock(nn.Module):
-    def __init__(self, embed_dim, max_seq_len, n_heads, hidden_size, has_cross_attention = True):
+    def __init__(self, embed_dim, max_seq_len, n_heads, hidden_size, has_cross_attention=True):
         super().__init__()
 
         self.has_cross_attention = has_cross_attention
 
-        self.self_attention = SelfAttention(embed_dim, max_seq_len, n_heads, is_causal=True)
+        self.self_attention = SelfAttention(
+            embed_dim, max_seq_len, n_heads, is_causal=True)
         if has_cross_attention:
             self.cross_attention = CrossAttention(embed_dim, n_heads)
+        self.feed_forward = FeedForward(embed_dim, hidden_size)
+
+    def forward(self, embeddings):
+
+        output = self.self_attention(embeddings)
+        if self.has_cross_attention:
+            output = self.cross_attention(output)
+        output = self.feed_forward(output)
+
+        return output
+
+
+class FlashDecoderBlock(nn.Module):
+    def __init__(self, embed_dim, max_seq_len, n_heads, hidden_size, has_cross_attention=True):
+        super().__init__()
+
+        self.has_cross_attention = has_cross_attention
+
+        self.self_attention = FlashSelfAttention(
+            embed_dim, n_heads, is_causal=True)
+        if has_cross_attention:
+            self.cross_attention = FlashCrossAttention(embed_dim, n_heads)
         self.feed_forward = FeedForward(embed_dim, hidden_size)
 
     def forward(self, embeddings):
